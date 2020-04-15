17e3d74ed6e18d8216e1952f9e008575138e447b
Asankilp <3384816498@qq.com>
2020-04-15 15:14:22 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,376 @@
+﻿namespace YXHGenerator_Desktop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.output1 = new System.Windows.Forms.TextBox();
+            this.generate1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.reason1 = new System.Windows.Forms.TextBox();
+            this.event1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.master1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.channel1 = new System.Windows.Forms.TextBox();
+            this.country1_1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.event2_1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.guocheng1 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.event2_2 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.country1_2 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.generate2 = new System.Windows.Forms.Button();
+            this.output2 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(1, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(809, 481);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.output1);
+            this.tabPage1.Controls.Add(this.generate1);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.reason1);
+            this.tabPage1.Controls.Add(this.event1);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.master1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(801, 455);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "事件解释";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // output1
+            // 
+            this.output1.Location = new System.Drawing.Point(14, 150);
+            this.output1.Multiline = true;
+            this.output1.Name = "output1";
+            this.output1.ReadOnly = true;
+            this.output1.Size = new System.Drawing.Size(607, 196);
+            this.output1.TabIndex = 7;
+            // 
+            // generate1
+            // 
+            this.generate1.Location = new System.Drawing.Point(14, 87);
+            this.generate1.Name = "generate1";
+            this.generate1.Size = new System.Drawing.Size(607, 24);
+            this.generate1.TabIndex = 6;
+            this.generate1.Text = "生成";
+            this.generate1.UseVisualStyleBackColor = true;
+            this.generate1.Click += new System.EventHandler(this.generate1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "解释";
+            // 
+            // reason1
+            // 
+            this.reason1.Location = new System.Drawing.Point(71, 60);
+            this.reason1.Name = "reason1";
+            this.reason1.Size = new System.Drawing.Size(550, 21);
+            this.reason1.TabIndex = 4;
+            // 
+            // event1
+            // 
+            this.event1.Location = new System.Drawing.Point(71, 33);
+            this.event1.Name = "event1";
+            this.event1.Size = new System.Drawing.Size(550, 21);
+            this.event1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "事件";
+            // 
+            // master1
+            // 
+            this.master1.Location = new System.Drawing.Point(71, 6);
+            this.master1.Name = "master1";
+            this.master1.Size = new System.Drawing.Size(550, 21);
+            this.master1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "事件主体";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label11);
+            this.tabPage2.Controls.Add(this.output2);
+            this.tabPage2.Controls.Add(this.generate2);
+            this.tabPage2.Controls.Add(this.country1_2);
+            this.tabPage2.Controls.Add(this.label10);
+            this.tabPage2.Controls.Add(this.event2_2);
+            this.tabPage2.Controls.Add(this.label9);
+            this.tabPage2.Controls.Add(this.guocheng1);
+            this.tabPage2.Controls.Add(this.label8);
+            this.tabPage2.Controls.Add(this.event2_1);
+            this.tabPage2.Controls.Add(this.label7);
+            this.tabPage2.Controls.Add(this.country1_1);
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.channel1);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(801, 455);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "外国小哥类型";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 126);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "输出";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 16);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 12);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "频道名";
+            // 
+            // channel1
+            // 
+            this.channel1.Location = new System.Drawing.Point(73, 13);
+            this.channel1.Name = "channel1";
+            this.channel1.Size = new System.Drawing.Size(252, 21);
+            this.channel1.TabIndex = 1;
+            this.channel1.Text = "营销号看世界";
+            // 
+            // country1_1
+            // 
+            this.country1_1.Location = new System.Drawing.Point(73, 49);
+            this.country1_1.Name = "country1_1";
+            this.country1_1.Size = new System.Drawing.Size(252, 21);
+            this.country1_1.TabIndex = 3;
+            this.country1_1.Text = "外国";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(14, 52);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 12);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "国籍";
+            // 
+            // event2_1
+            // 
+            this.event2_1.Location = new System.Drawing.Point(73, 87);
+            this.event2_1.Multiline = true;
+            this.event2_1.Name = "event2_1";
+            this.event2_1.Size = new System.Drawing.Size(252, 80);
+            this.event2_1.TabIndex = 5;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(14, 90);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 12);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "具体事件";
+            // 
+            // guocheng1
+            // 
+            this.guocheng1.Location = new System.Drawing.Point(73, 184);
+            this.guocheng1.Multiline = true;
+            this.guocheng1.Name = "guocheng1";
+            this.guocheng1.Size = new System.Drawing.Size(252, 109);
+            this.guocheng1.TabIndex = 7;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(14, 187);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 12);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "具体过程";
+            // 
+            // event2_2
+            // 
+            this.event2_2.Location = new System.Drawing.Point(73, 312);
+            this.event2_2.Multiline = true;
+            this.event2_2.Name = "event2_2";
+            this.event2_2.Size = new System.Drawing.Size(252, 43);
+            this.event2_2.TabIndex = 9;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(14, 315);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(53, 12);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "简略事件";
+            // 
+            // country1_2
+            // 
+            this.country1_2.Location = new System.Drawing.Point(73, 373);
+            this.country1_2.Name = "country1_2";
+            this.country1_2.Size = new System.Drawing.Size(252, 21);
+            this.country1_2.TabIndex = 11;
+            this.country1_2.Text = "日本";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(14, 376);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(53, 12);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "第二国籍";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // generate2
+            // 
+            this.generate2.Location = new System.Drawing.Point(16, 400);
+            this.generate2.Name = "generate2";
+            this.generate2.Size = new System.Drawing.Size(309, 28);
+            this.generate2.TabIndex = 12;
+            this.generate2.Text = "生成";
+            this.generate2.UseVisualStyleBackColor = true;
+            this.generate2.Click += new System.EventHandler(this.generate2_Click);
+            // 
+            // output2
+            // 
+            this.output2.Location = new System.Drawing.Point(353, 28);
+            this.output2.Multiline = true;
+            this.output2.Name = "output2";
+            this.output2.ReadOnly = true;
+            this.output2.Size = new System.Drawing.Size(323, 385);
+            this.output2.TabIndex = 13;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(351, 3);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(29, 12);
+            this.label11.TabIndex = 14;
+            this.label11.Text = "输出";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 478);
+            this.Controls.Add(this.tabControl1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(816, 517);
+            this.MinimumSize = new System.Drawing.Size(816, 517);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "营销号生成器";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TextBox event1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox master1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button generate1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox reason1;
+        private System.Windows.Forms.TextBox output1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox country1_2;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox event2_2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox guocheng1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox event2_1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox country1_1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox channel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button generate2;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox output2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace YXHGenerator_Desktop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void generate1_Click(object sender, EventArgs e)
+        {
+            String mastervar1 = master1.Text; //事件主体
+            String eventvar1 = event1.Text; //事件
+            String reasonvar1 = reason1.Text; //解释
+            output1.Text = "\t" + mastervar1 + eventvar1 + "是怎么回事呢？" + mastervar1 + "相信大家都很熟悉，但是" + mastervar1 + eventvar1 + "是怎么回事呢，下面就让小编带大家一起了解吧。"
+                + mastervar1 + eventvar1 + "，其实就是" + reasonvar1 + "。大家可能会很惊讶，" + mastervar1 + "怎么会" + eventvar1 + "呢？但事实就是这样，小编也感到非常惊讶。"
+                + "这就是关于" + mastervar1 + eventvar1 + "的事情了，大家有什么想法呢，欢迎在评论区告诉小编一起讨论哦！";
+        }
+
+        private void generate2_Click(object sender, EventArgs e)
+        {
+            String channelvar1;
+            channelvar1 = channel1.Text; //频道名
+            String countryvar1_1;
+             countryvar1_1 = country1_1.Text;  //第一国籍
+            String eventvar2_1 = event2_1.Text; //具体事件
+            String guochengvar1 = guocheng1.Text; //具体过程
+            String eventvar2_2 = event2_2.Text; //简略事件
+            String countryvar1_2;
+            countryvar1_2 = country1_2.Text;//第二国籍
+
+            output2.Text = "\t" + "大家好，这里是" + channelvar1 + "，近日，一位" + countryvar1_1 + "小哥" + eventvar2_1 + "，让我们来看看他是怎么做的吧。"
+            + "可以看见，他" + guochengvar1 + "，这样就完成了整个" + eventvar2_2 + "的过程，大家是不是非常惊讶？" + countryvar1_1 + "小哥简直太厉害了，轻松做到了" + eventvar2_2 + "，就连小编也感到特别惊讶。"
+            + "大家还想再看到更多关于" + countryvar1_2 + "小哥的有趣的事情吗？欢迎订阅我的频道。我是" + channelvar1 + "，我们下期再见！";
+        }
+    }
+}
